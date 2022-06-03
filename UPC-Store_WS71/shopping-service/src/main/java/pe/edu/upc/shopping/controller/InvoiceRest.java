722e5c536e32919a121f8fb448457139a6e94f5f
JuanPabloPT2001 <u201915206@gmail.com>
2022-06-03 01:41:41 -0500

    aea
    g

---
+++ UPC-Store_WS71/shopping-service/src/main/java/pe/edu/upc/shopping/controller/InvoiceRest.java
@@ -0,0 +1,79 @@
+package pe.edu.upc.shopping.controller;
+
+import pe.edu.upc.shopping.service.InvoiceService;
+import pe.edu.upc.shopping.entity.Invoice;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceRest {
+
+    @Autowired
+    InvoiceService invoiceService;
+
+    // -------------------Retrieve All Invoices--------------------------------------------
+    @GetMapping
+    public ResponseEntity<List<Invoice>> listAllInvoices() {
+        return null;
+    }
+
+    // -------------------Retrieve Single Invoice------------------------------------------
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Invoice> getInvoice(@PathVariable("id") long id) {
+        return null;
+    }
+
+    // -------------------Create a Invoice-------------------------------------------
+    @PostMapping
+    public ResponseEntity<Invoice> createInvoice(@Valid @RequestBody Invoice invoice, BindingResult result) {
+        return null;
+    }
+
+    // ------------------- Update a Invoice ------------------------------------------------
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> updateInvoice(@PathVariable("id") long id, @RequestBody Invoice invoice) {
+        return null;
+    }
+
+    // ------------------- Delete a Invoice-----------------------------------------
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Invoice> deleteInvoice(@PathVariable("id") long id) {
+        return null;
+    }
+
+    private String formatMessage( BindingResult result){
+        List<Map<String,String>> errors = result.getFieldErrors().stream()
+                .map(err ->{
+                    Map<String,String> error =  new HashMap<>();
+                    error.put(err.getField(), err.getDefaultMessage());
+                    return error;
+
+                }).collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString="";
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+}
